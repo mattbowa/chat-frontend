56a31a29ffc2af36ff5990566aa1afbd030bb52d
Matt Bowa <matt.bowa04@gmail.com>
2026-06-08 01:41:34 +1000

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000",
   },
